61eb5f95aa3123b8a2261bfc93125a4ff58d5290
rnycz <radeknycz98@gmail.com>
2022-07-01 13:02:40 +0200

    code refractor

--- script.js
+++ script.js
@@ -1,142 +1,128 @@
-function Calculate() {
-    var billAmount = document.getElementById("billAmount").value;
-    var serviceRate = document.getElementById("serviceRate").value;
-    var peopleNumber = document.getElementById("peopleNumber").value;
-    var infoTip = document.getElementById("total-tip");
+const calculate = () =>{
+  const billAmount = document.querySelector("#billAmount").value;
+  const serviceRate = document.querySelector("#serviceRate").value;
+  const peopleNumber = document.querySelector("#peopleNumber").value;
+  const infoTip = document.querySelector("#total-tip");
   
-    var totalTip = (billAmount * serviceRate) / peopleNumber;
-    totalTip = Math.round(totalTip * 100) / 100;
+  let totalTip = (billAmount * serviceRate) / peopleNumber;
+  totalTip = Math.round(totalTip * 100) / 100;
   
-    if (peopleNumber > 1) {
-      infoTip.innerHTML = "<b>Tip amount: " + totalTip + " $ each</b>";
-      infoTip.style.color = "black";
-    } else {
-      infoTip.innerHTML = "<b>Tip amount: " + totalTip + " $</b>";
-      infoTip.style.color = "black";
-    }
+  if (peopleNumber > 1) {
+    infoTip.innerHTML = `<b>Tip amount: ${totalTip} $ each</b>`;
+    infoTip.style.color = "black";
+  } else {
+    infoTip.innerHTML = `<b>Tip amount: ${totalTip} $</b>`;
+    infoTip.style.color = "black";
+  }
+
+  if (peopleNumber == "" || peopleNumber <= 0) {
+    infoTip.innerHTML = "Enter the number of people";
+    infoTip.style.color = "rgb(161, 7, 7)";
+  }
   
-    if (peopleNumber == "" || peopleNumber <= 0) {
-      infoTip.innerHTML = "Enter the number of people";
+  if (serviceRate == 1) {
+    infoTip.innerHTML = "Choose the quality of service";
       infoTip.style.color = "rgb(161, 7, 7)";
-    }
+  }
   
-    if (serviceRate == 1) {
-      infoTip.innerHTML = "Choose the quality of service";
-      infoTip.style.color = "rgb(161, 7, 7)";
-    }
+  if (billAmount == "" || billAmount <= 0) {
+    infoTip.innerHTML = "Enter the value of the bill";
+    infoTip.style.color = "rgb(161, 7, 7)";
+  }
   
-    if (billAmount == "" || billAmount <= 0) {
-      infoTip.innerHTML = "Enter the value of the bill";
-      infoTip.style.color = "rgb(161, 7, 7)";
-    }
-  
-    if (serviceRate == 0) {
-      infoTip.innerHTML = "You do not want to tip";
-      infoTip.style.color = "black";
-    }
-    console.log(innerWidth);
-  };
-  /*function BlinkText(val){
-      setInterval(function(){
-          val.style.color = (val.style.color == 'black' ? 'red' : 'black');
-      }, 1000);
-      return;
-  }*/
-  function showTime() {
-    var date = new Date();
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var seconds = date.getSeconds();
-    hours = checkTime(hours);
-    minutes = checkTime(minutes);
-    seconds = checkTime(seconds);
-    var time = hours + ":" + minutes + ":" + seconds;
-    document.getElementById("timer").innerHTML = "Current time: " + time;
+  if (serviceRate == 0) {
+    infoTip.innerHTML = "You do not want to tip";
+    infoTip.style.color = "black";
+  }
+}
+document.querySelector("#calculate").addEventListener("click", calculate);
+
+const checkTime = (i) =>{
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+const showTime = () =>{
+  const date = new Date();
+  const hours = checkTime(date.getHours());
+  const minutes = checkTime(date.getMinutes());
+  const seconds = checkTime(date.getSeconds());
+  const time = `${hours}:${minutes}:${seconds}`;
+    document.querySelector("#timer").innerHTML = `Current time: ${time}`;
     setTimeout("showTime()", 100);
-  };
+};
+showTime();
+
   
-  function checkTime(i) {
-    if (i < 10) {
-      i = "0" + i;
-    }
-    return i;
+const countdownDate = new Date("Mar 30, 2023 00:00:00").getTime();
+const x = setInterval(() =>{
+  const now = new Date().getTime();
+  const count = countdownDate - now;
+  const days = Math.floor(count / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((count % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((count % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((count % (1000 * 60)) / 1000);
+  document.querySelector("#expire-coupon1").innerHTML = `Coupon expires in:
+    ${days}d ${hours}h ${minutes}m ${seconds}s`;
+  document.querySelector("#expire-coupon2").innerHTML = `Coupon expires in:
+    ${days}d ${hours}h ${minutes}m ${seconds}s`;
+  if (count < 0) {
+    clearInterval(x);
+    document.querySelector("#expire-coupon1").innerHTML = "Coupon expired";
+    document.querySelector("#expire-coupon2").innerHTML = "Coupon expired";
   }
+}, 1000);
   
-  var countDownDate = new Date("Mar 30, 2021 00:00:00").getTime();
-  var x = setInterval(function() {
-    var now = new Date().getTime();
-    var count = countDownDate - now;
-    var days = Math.floor(count / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((count % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((count % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((count % (1000 * 60)) / 1000);
-    document.getElementById("expire-coupon1").innerHTML = "Coupon expires in: " +
-      days + "d " + hours + "h " + minutes + "m " + seconds + "s";
-    document.getElementById("expire-coupon2").innerHTML = "Coupon expires in: " +
-      days + "d " + hours + "h " + minutes + "m " + seconds + "s";
-    if (count < 0) {
-      clearInterval(x);
-      document.getElementById("expire-coupon1").innerHTML = "Coupon expired";
-      document.getElementById("expire-coupon2").innerHTML = "Coupon expired";
-    }
-  }, 1000);
+const scrollProgress = () =>{
+  const windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrolled = (windowScroll / height) * 100;
+  document.querySelector("#progressBar").style.width = `${scrolled}%`;
+};
   
-  function scrollProgres() {
-    var windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (windowScroll / height) * 100;
-    document.getElementById("progressBar").style.width = scrolled + "%";
-  };
+const stickyMenu = () =>{
+  const menu = document.querySelector("#navbar");
+  const header = document.querySelector("#image-header");
+  const sticky = menu.offsetTop; 
+  if (window.pageYOffset >= sticky) {
+    menu.classList.add("sticky-nav")
+    header.style.borderBottom = "5px solid black";
+  } 
+}
+
+window.onscroll = function() {
+  scrollProgress();
+  stickyMenu();
+}
   
-  window.onscroll = function() {
-    scrollProgres();
-    stickyMenu();
-  };
+const openModal = () =>{
+  document.querySelector("#modalDiv").style.display = "block";
+}
+document.querySelector("#modalBtn").addEventListener("click", openModal)
   
+const closeModal = () =>{
+  document.querySelector("#modalDiv").style.display = "none";
+}
+document.querySelector("#closeModal").addEventListener("click", closeModal)
   
-  function stickyMenu() {
-    var menu = document.getElementById("navbar");
-    var header = document.getElementById("image-header");
-    var sticky = menu.offsetTop; 
-    if (window.pageYOffset >= sticky) {
-      menu.classList.add("sticky-nav")
-      header.style.borderBottom = "5px solid black";
-    } 
-  }
+const openMobile = () =>{
+  document.getElementById("mobile").style.width = "100%";
+}
+document.querySelector("#openMobile").addEventListener("click", openMobile)
   
-  function openModal() {
-    document.getElementById("modalDiv").style.display = "block";
-  }
+const closeMobile = () =>{
+  document.getElementById("mobile").style.width = "0%";
+}
+document.querySelector("#closeMobile").addEventListener("click", closeMobile)
   
-  function closeModal() {
-    document.getElementById("modalDiv").style.display = "none";
-  }
-  
-  function closeModalOnWindowClick(event) {
-    if (event.target == document.getElementById("modalDiv")) {
-      document.getElementById("modalDiv").style.display = "none";
-    }
-  }
-  
-  function openMobile() {
-    document.getElementById("mobile").style.width = "100%";
-  }
-  
-  function closeMobile() {
-    document.getElementById("mobile").style.width = "0%";
-  }
-  
-  function menuItemClicked() {
-    var i;
-    var clicked = document.getElementsByClassName("isClicked");
-    for (i = 0; i < clicked.length; i++) {
-      clicked[i].onclick = function() {
-        closeMobile();
-      }
+const menuItemClicked = () =>{
+  const clicked = document.querySelectorAll(".isClicked");
+  for (let i = 0; i < clicked.length; i++) {
+    clicked[i].onclick = () =>{
+      closeMobile();
     }
   }
-  window.onclick = function() {
-    menuItemClicked();
-    closeModalOnWindowClick();
-  }
-  
+}
+menuItemClicked();
